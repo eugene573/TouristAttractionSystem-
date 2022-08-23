02471511dcd3a03ad314bd33b1bb484672aee654
eugene573 <eugenefong2002@gmail.com>
2022-08-23 16:31:53 +0800

    23/8

--- resources/views/blog/commentsDisplay.blade.php
+++ resources/views/blog/commentsDisplay.blade.php
@@ -9,10 +9,17 @@
 
 @foreach($comments as $comment)
     <div class="display-comment" @if($comment->parent_id != null) @endif>
-        <a href="{{ $post->user->avatar }}"><img class="rounded-circle" height="70" width="70" style="float:left;"
-        src="{{$post->user->avatar}}" alt="User profile picture"></a>
-        <strong style="font-style: italic;font-size: 1.20rem; font-weight:bold; float:left;">{{ $comment->user->name }}
-        <span style="color: gray;font-style: italic;font-size: small;">{{ $comment->created_at->diffForHumans() }}</span></strong>
+        <a href="{{ $comment->user->avatar }}"><img class="rounded-circle" height="70" width="70" style="float:left;"
+        src="{{ $comment->user->avatar }}" alt="User profile picture"></a>
+        
+        
+        
+        <strong style="font-size: 1.20rem; font-weight:bold; float:left;">{{ $comment->user->name }}
+        &nbsp; 
+        &nbsp; 
+        <br>
+        &nbsp; 
+        <span style="color: gray;font-size: small; font-style:italic;">{{ $comment->created_at->diffForHumans() }}</span></strong>
         <br><br><br>
         <p style="float:left; text-align: justify;">{{ $comment->body }}</p>
         <a href="" id="reply"></a>
@@ -28,5 +35,6 @@
             </div>
         </form>
         @include('blog.commentsDisplay', ['comments' => $comment->replies])
+       <br><br>
     </div>
 @endforeach
